8d773685f18922403fc51be15edade0f3ced3a6a
asnike <asnike@notefolio.net>
2016-08-24 15:41:53 +0900

    * init

---
+++ app/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Router, Route, Link, browserHistory, IndexRedirect } from 'react-router';
+import App from './components/App';
+
+
+export default (
+	<Route path="/" component={App}>
+		
+	</Route>
+	
+)
